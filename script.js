d1f80d889b02078880b7ef439c89c4d6c073d786
mdalifol <micheldalifol@gmail.com>
2017-06-01 15:47:28 +0200

    modif1 js

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ exercice git à 3
 */
 
 
+/*
+on fait apparaitre les images une par une à chaque click sur le body
+
+	1. on voit s'il y a l image
+		1.1 si non => on la fait apparaitre
+		1.2 si oui on fait apparaitre la suivante
+*/
+
+//création d'une variable pour voir si l'image est affichée
+		var img = false; 	//on considère qu'elle n'apparait pas
 
 
+		$('body').click(function() {
 
+			
+
+				//voir si l'image est display none
+				if (img === false) {
+					$('#html').fadeIn(500);
+				}
+
+				
+		});
