d38d93ef81b5919556c04ca9434ea70ce18d3eee
mdalifol <micheldalifol@gmail.com>
2017-06-01 16:20:17 +0200

    modif3 js

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ exercice git à 3
 */
 
 
-/*
-on fait apparaitre les images une par une à chaque click sur le body
 
-	1. on voit s'il y a l image
-		1.1 si non => on la fait apparaitre
-		1.2 si oui on fait apparaitre la suivante
-*/
 
 //création d'une variable pour voir si l'image est affichée
 		var i = 1 ;	
